4b15ea0cb0dd4b2b6e38c774c15eabf029d19ba1
lysolha <rdnv.ol20@gmail.com>
2025-07-19 16:31:40 +0300

    feat: update package.json dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { router } from './assets/routes';
 
 function App() {
   return (
-    <>
+    <div className="max-w-screen-xl mx-auto px-4 py-10">
       <RouterProvider router={router} />
-    </>
+    </div>
   );
 }
 
