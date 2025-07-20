8f6ff66f814568a0057aa5ceb5cae040a68ea0d7
lysolha <rdnv.ol20@gmail.com>
2025-07-20 12:57:17 +0300

    feat: create user auth logic

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,104 @@
+import { Form, Formik } from 'formik';
+import { useState } from 'react';
+import * as Yup from 'yup';
+import EyeIcon from '../assets/icons/eye-svgrepo-com.svg';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+interface AuthFormProps {
+  method: 'login' | 'register';
+  validationSchema: Yup.ObjectSchema<object>;
+  handleSubmit: (values: {
+    email: string;
+    password: string;
+    name: string;
+    confirmPassword: string;
+  }) => void;
+}
+
+const AuthForm = ({
+  method,
+  validationSchema,
+  handleSubmit,
+}: AuthFormProps) => {
+  const initialValues = {
+    email: '',
+    password: '',
+    name: '',
+    confirmPassword: '',
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log(values);
+        handleSubmit(values);
+      }}
+      validateOnBlur={true}
+      validateOnChange={true}
+    >
+      {({ dirty, isValid }) => (
+        <Form className="flex flex-col gap-4 items-center justify-center">
+          <Input
+            name="name"
+            label="Name"
+            placeholder="Name"
+            disabled={method === 'login'}
+            className={method === 'login' ? 'hidden' : ''}
+          />
+          <Input name="email" label="Email" placeholder="Email" />
+          <Input
+            name="password"
+            label="Password"
+            placeholder="Password"
+            type={showPassword ? 'text' : 'password'}
+            icon={
+              showPassword ? (
+                <img width={20} height={20} src={EyeIcon} alt="Show password" />
+              ) : (
+                <img
+                  width={20}
+                  height={20}
+                  src={EyeIcon}
+                  alt="Show password"
+                  className="opacity-50"
+                />
+              )
+            }
+            onClick={() => setShowPassword(!showPassword)}
+          />
+          <Input
+            name="confirmPassword"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            disabled={method === 'login'}
+            className={method === 'login' ? 'hidden' : ''}
+            icon={
+              showPassword ? (
+                <img width={20} height={20} src={EyeIcon} alt="Show password" />
+              ) : (
+                <img
+                  width={20}
+                  height={20}
+                  src={EyeIcon}
+                  alt="Show password"
+                  className="opacity-50"
+                />
+              )
+            }
+            onClick={() => setShowPassword(!showPassword)}
+          />
+          <Button type="submit" disabled={!dirty || !isValid}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default AuthForm;
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,120 @@
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
+import AuthForm from '../components/AuthForm';
+import { createUser, loginUser } from '../store/extraReducers/userExtraReduсer';
+import type { AppDispatch } from '../store/store';
+
 const Auth = () => {
+  const [method, setMethod] = useState<'login' | 'register'>('login');
+  const [validationSchema, setValidationSchema] = useState(Yup.object({}));
+  const registerRef = useRef<HTMLAnchorElement>(null);
+  const loginRef = useRef<HTMLAnchorElement>(null);
+  const handleMethodChange = (method: 'login' | 'register') => {
+    setMethod(method);
+    if (method === 'register') {
+      setValidationSchema(
+        Yup.object({
+          name: Yup.string().required('Required'),
+          email: Yup.string().email('Invalid email').required('Required'),
+          password: Yup.string()
+            .required('Required')
+            .min(8, 'Password must be at least 8 characters long'),
+          confirmPassword: Yup.string()
+            .required('Required')
+            .oneOf([Yup.ref('password')], 'Passwords must match'),
+        })
+      );
+    } else {
+      setValidationSchema(
+        Yup.object({
+          email: Yup.string().email('Invalid email').required('Required'),
+          password: Yup.string()
+            .min(8, 'Password must be at least 8 characters long')
+            .required('Required'),
+        })
+      );
+    }
+  };
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const handleSubmit = async (values: {
+    email: string;
+    password: string;
+    name?: string;
+    confirmPassword?: string;
+  }) => {
+    if (method === 'register') {
+      try {
+        const token = await dispatch(
+          createUser({
+            name: values.name || '',
+            email: values.email,
+            password: values.password,
+            confirmPassword: values.confirmPassword || '',
+          })
+        ).unwrap();
+        if (token) {
+          navigate('/');
+          toast.success('Registered successfully');
+        }
+      } catch (error) {
+        toast.error((error as { code?: string }).code || 'Registration failed');
+      }
+    } else {
+      try {
+        const token = await dispatch(loginUser(values)).unwrap();
+        if (token) {
+          navigate('/');
+          toast.success('Logged in successfully');
+        }
+      } catch (error) {
+        toast.error((error as { code?: string }).code || 'Login failed');
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleMethodChange(method);
+  }, [method]);
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-1/2 min-h-1/2 border border-gray-300 rounded-md p-4 shadow-md">
+        <h1 className="text-2xl font-bold text-center mb-4">Films editor</h1>
+        <h2 className="text-center mb-4"> {method}</h2>
+
+        <AuthForm
+          key={method}
+          method={method}
+          validationSchema={validationSchema}
+          handleSubmit={handleSubmit}
+        />
+        <div className="flex gap-4 justify-center">
+          <a
+            ref={registerRef}
+            href="#"
+            className={`text-blue-500 ${
+              method === 'register' ? 'text-red-500' : 'text-blue-500'
+            }`}
+            onClick={() => setMethod('register')}
+          >
+            Register
+          </a>
+          <a
+            ref={loginRef}
+            href="#"
+            className={`text-blue-500 ${
+              method === 'login' ? 'text-red-500' : 'text-blue-500'
+            }`}
+            onClick={() => setMethod('login')}
+          >
+            Login
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
