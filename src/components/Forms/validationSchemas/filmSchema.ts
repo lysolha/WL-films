5678c0a781c99f8a76745d30e491de639d8175cb
lysolha <rdnv.ol20@gmail.com>
2025-07-21 00:50:37 +0300

    feat: created film schema for film form

---
+++ src/components/Forms/validationSchemas/filmSchema.ts
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+const filmSchema = Yup.object({
+  title: Yup.string()
+    .max(30, 'Must be 30 characters or less')
+    .required('Title is required'),
+  year: Yup.number()
+    .min(1900, 'Must be greater than 1900')
+    .max(2021, 'Must be less than 2021')
+    .required('Year is required'),
+  format: Yup.string()
+    .max(10, 'Must be 10 characters or less')
+    .required('Format is required'),
+  actors: Yup.array().of(
+    Yup.string()
+      .max(30, 'Must be 30 characters or less')
+      .required('Actor is required')
+  ),
+});
+
+export default filmSchema;
